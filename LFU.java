a9b848ef1a54490268e8174c126b6087f35ac214
JThai <admin@AZ-MacBook-Pro.local>
2017-04-12 03:24:42 -0700

    A HOUSE IN NEW ORLEANS, THEY CALL THE RISING SUNNNN. AND IT'S BEEN THE RUIN OF MANY A POOR BOY, AND GOD I KNOW, I'M ONE

--- LFU.java
+++ LFU.java
@@ -22,14 +22,16 @@ public class LFU extends PagingAlgo {
 		int currIndex = new Random().nextInt(DISK_FRAMES.length);
 		int counter = 0;
 
-		while (counter < SIMULATION_TIME) { //SIMULATION_TIME
+		while (counter < SIMULATION_TIME) { // SIMULATION_TIME
 			boolean pageFault = true;
 			currPage = DISK_FRAMES[currIndex];
 			pageFault = findPageFault(pageBuffer, currPage);
 			if (pageFault == false) // No Page Fault
 			{
 				int loc = getFirstFreeSpace(pageBuffer);
-				if (loc != -1 && notInBuffer(pageBuffer, currPage) == true) { // pageBuffer is full
+				if (loc != -1 && notInBuffer(pageBuffer, currPage) == true) { // pageBuffer
+																				// is
+																				// full
 					pageBuffer[loc] = currPage;
 				}
 				int value = frequencies.get(currPage) + 1;
@@ -54,7 +56,9 @@ public class LFU extends PagingAlgo {
 			counter++;
 		}
 	}
+
 	/**
+	 * Checks if it's in the page buffer or not
 	 * 
 	 * @param pageBuffer2
 	 * @param currPage
@@ -63,36 +67,38 @@ public class LFU extends PagingAlgo {
 	private boolean notInBuffer(Character[] list, char currPage) {
 		// TODO Auto-generated method stub
 		ArrayList<Character> checking = new ArrayList<Character>();
-		for (int i = 0; i < list.length; i++)
-		{
+		for (int i = 0; i < list.length; i++) {
 			checking.add(list[i]);
 		}
-		if (!checking.contains(currPage)) return true;
+		if (!checking.contains(currPage))
+			return true;
 		return false;
 	}
 
+	/**
+	 * Remove Handling and putting in correctly
+	 */
 	private void removeChar(Character chosen, Character currPage, Character[] list) {
 		// TODO Auto-generated method stub
-		
+
 		int index = 0;
-		if (getFirstFreeSpace(list) == -1){
+		if (getFirstFreeSpace(list) == -1) {
 			index = findPage(pageBuffer, chosen);
 			// remove char from arrival order
 			for (int i = 0; i < arrivalInOrder.size(); i++) {
-				if (arrivalInOrder.get(i).equals(chosen)) arrivalInOrder.remove(i);
+				if (arrivalInOrder.get(i).equals(chosen))
+					arrivalInOrder.remove(i);
 			}
 			// Change Frequencies
 			frequencies.put(chosen, 0);
-		}
-		else
-		{
+		} else {
 			index = getFirstFreeSpace(list);
 		}
 		int value = 0;
 		value = frequencies.get(currPage) + 1; // make sure it's Zero
-		frequencies.put(currPage,value);
+		frequencies.put(currPage, value);
 		// Change Page Buffer
-		if (notInBuffer(list,currPage) == true)
+		if (notInBuffer(list, currPage) == true)
 			pageBuffer[index] = currPage;
 	}
 
@@ -114,6 +120,13 @@ public class LFU extends PagingAlgo {
 		return false;
 	}
 
+	/**
+	 * Finds the Page
+	 * 
+	 * @param list
+	 * @param x
+	 * @return Finds Page within Page bufer
+	 */
 	private int findPage(Character[] list, char x) {
 		// TODO Auto-generated method stub
 		int val = 0;
@@ -124,16 +137,30 @@ public class LFU extends PagingAlgo {
 		return val;
 	}
 
+	/**
+	 * Finds Page Fault
+	 * 
+	 * @param list
+	 * @param currPage
+	 * @return false if no page fault, true if there is
+	 */
 	private boolean findPageFault(Character[] list, char currPage) {
 		// TODO Auto-generated method stub
 		ArrayList<Character> temp = new ArrayList<Character>();
 		for (int i = 0; i < list.length; i++) {
 			temp.add(list[i]);
 		}
-		if (temp.contains(currPage)) return false;
+		if (temp.contains(currPage))
+			return false;
 		return true;
 	}
 
+	/**
+	 * Finds First Free space
+	 * 
+	 * @param list
+	 * @return First instance of Free Space
+	 */
 	private int getFirstFreeSpace(Character[] list) {
 		// TODO Auto-generated method stub
 		for (int i = 0; i < list.length; i++) {
@@ -148,7 +175,7 @@ public class LFU extends PagingAlgo {
 	 * 
 	 * @param frequencies
 	 * @param list
-	 * @return
+	 * @return Least Frequency Location
 	 */
 	public char getLeastFrequency(Map<Character, Integer> frequencies, ArrayList<Character> list) {
 		Integer[] values = frequencies.values().toArray(new Integer[frequencies.size()]);
